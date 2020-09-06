5988024a9037904b29d9c5f908736cf66a53d748
Oswaldo Andrés <48687178+Oswalman@users.noreply.github.com>
2020-09-06 15:47:25 -0500

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app>
+    <v-app-bar color="white" max-height="60px" height="60px" dense>
+      <v-btn to="/" text>
+        INICIO
+      </v-btn>
+      <v-spacer></v-spacer>
+
+      <v-btn to="/services" text>
+        SERVICIOS
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn to="/services" text>
+        Tienda
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
+  
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: "mdi-apps",
+          title: "Welcome",
+          to: "/"
+        },
+        {
+          icon: "mdi-chart-bubble",
+          title: "Inspire",
+          to: "/inspire"
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: "Vuetify.js"
+    };
+  }
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container > 
+    <v-carousel >
+
+    </v-carousel>
+
+
+  </v-container>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+import VuetifyLogo from "~/components/VuetifyLogo.vue";
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+};
+</script>
